5171e43d8ae7ab2875c30c584dda00e4ff5d4e24
remichartier <remipr.chartier@gmail.com>
2021-03-05 06:32:31 +0000

    init again due to issues with .git

---
+++ README.md
@@ -0,0 +1,2 @@
+# 024_selfDrivingCarND_ROS_LabPractice_ArmMover
+Introduction to ROS usage, dev. of an arm mover robot
---
+++ bashrc.sh
@@ -0,0 +1,7 @@
+git config --global user.email "remipr.chartier@gmail.com"
+git config --global user.name "remichartier"
+git remote set-url origin "git@github.com:remichartier/024_selfDrivingCarND_ROS_LabPractice_ArmMover.git"
+apt update
+apt install openssh-client
+eval "$(ssh-agent -s)"
+ssh-add .ssh/20210214_1219_github_id_ed25519
